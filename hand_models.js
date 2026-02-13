5f616f8858c0eeb8f0ca67083c66da4d10ca45ea
kerl <coolbloxofficial@gmail.com>
2026-02-13 17:00:32 -0500

    Create hand_models.js

---
+++ hand_models.js
@@ -0,0 +1,32 @@
+{
+    "models": [
+        {
+            "id": "classic_hand",
+            "name": "Classic Hand",
+            "path": "/hand.glb",
+            "thumbnail": null,
+            "boneAxis": "z"
+        },
+              {
+            "id": "mario",
+            "name": "Mario Hand",
+            "path": "/mariohand.glb",
+            "thumbnail": null,
+            "boneAxis": "z"
+        },
+              {
+            "id": "freddy",
+            "name": "Freddy Hand",
+            "path": "/fredhand.glb",
+            "thumbnail": null,
+            "boneAxis": "z"
+        },
+                    {
+            "id": "real",
+            "name": "Realistic Hand",
+            "path": "/realistichand.glb",
+            "thumbnail": null,
+            "boneAxis": "z"
+        }
+    ]
+}
